d485317fd20efaf3429a16efb64b082a3bdbd20c
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-06-21 11:34:56 +0200

    feat(home): add landing page with public usernames

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
 
 namespace MVC.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly ApplicationDbContext _context;
+    public HomeController(ApplicationDbContext context)
     {
-        return View();
+        _context = context;
+    }
+    public async Task<IActionResult> Index()
+    {
+        var model = new HomeIndexViewModel
+        {
+            UserNames = await _context.Users.Select(t => t.UserName!).ToListAsync(),
+        };
+        return View(model);
     }
 }
---
+++ MVC/Models/HomeIndexViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace MVC.Models;
+
+public class HomeIndexViewModel
+{
+    public List<string> UserNames { get; set; } = new List<string>();
+}
