0340776425bd91a64e38b6d8077c1c4c41c93e90
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-06-25 18:31:06 +0200

    feat(home): redirect signed in users to my logs page

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -1,23 +1,35 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models;
+using MVC.Models.Entities;
 
 namespace MVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ApplicationDbContext _context;
-    public HomeController(ApplicationDbContext context)
+    private readonly SignInManager<ApplicationUser> _signInManager;
+    public HomeController(ApplicationDbContext context, SignInManager<ApplicationUser> signInManager)
     {
         _context = context;
+        _signInManager = signInManager;
     }
     public async Task<IActionResult> Index()
     {
+        var isSignedIn = _signInManager.IsSignedIn(User);
+
+        if (isSignedIn)
+        {
+            return RedirectToAction("Index", "DailyLogs");
+        }
+
         var model = new HomeIndexViewModel
         {
             UserNames = await _context.Users.Select(t => t.UserName!).ToListAsync(),
         };
+
         return View(model);
     }
 }
