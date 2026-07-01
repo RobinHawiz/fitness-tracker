0506ba860888560629fabfcfd006438c2b2a4ef0
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-07-01 10:19:03 +0200

    feat(client): add assigned clients page

---
+++ MVC/Controllers/ClientController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC.Data;
+using MVC.Models;
+using MVC.Models.Entities;
+
+namespace MVC.Controllers;
+
+[Authorize]
+public class ClientController : Controller
+{
+    private readonly ApplicationDbContext _context;
+    private UserManager<ApplicationUser> _userManager;
+
+    public ClientController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var assignedClients = await _context.CoachAssignments
+            .Where(ca => ca.CoachId == userId)
+            .Select(ca => new AssignedClientViewModel
+            {
+                Id = ca.Id,
+                UserName = ca.Member.UserName!
+            }).ToListAsync();
+
+        return View(assignedClients);
+    }
+
+    [HttpPost("client/{id:int}/remove")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Remove(int id)
+    {
+        var userId = _userManager.GetUserId(User);
+
+        var assignedClient = await _context.CoachAssignments
+            .FirstOrDefaultAsync(d => d.CoachId == userId && d.Id == id);
+
+        if (assignedClient == null)
+        {
+            return NotFound();
+        }
+
+        _context.CoachAssignments.Remove(assignedClient);
+
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ MVC/Models/AssignedClientViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC.Models;
+
+public class AssignedClientViewModel
+{
+    public int Id { get; set; }
+    public string UserName { get; set; } = default!;
+}
