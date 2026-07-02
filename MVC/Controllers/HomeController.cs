d2e2ad66b846d2f35382c52759b138bc11c1c62d
RobinHawiz <Robin.Hawiz.6670@student.uu.se>
2026-07-02 11:38:41 +0200

    refactor(home): move date helpers to extensions

--- MVC/Controllers/HomeController.cs
+++ MVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC.Data;
 using MVC.Models;
+using MVC.Extensions;
 using MVC.Models.Entities;
 
 namespace MVC.Controllers;
@@ -122,31 +123,4 @@ public class HomeController : Controller
         return View(model);
     }
 
-}
-
-public static class DateTimeExtensions
-{
-    public static DateOnly FirstDayOfWeek(this DateOnly dt)
-    {
-        // Assume Monday is the first day of the week.
-        // Sunday enum value is 0, so we set it to 6 to get the correct difference.
-        var diff = dt.DayOfWeek == DayOfWeek.Sunday ? 6 : (int)dt.DayOfWeek - (int)DayOfWeek.Monday;
-
-        return dt.AddDays(-diff);
-    }
-
-    public static DateOnly LastDayOfWeek(this DateOnly dt) =>
-        dt.FirstDayOfWeek().AddDays(6);
-
-    public static DateOnly FirstDayOfMonth(this DateOnly dt) =>
-        new(dt.Year, dt.Month, 1);
-
-    public static DateOnly LastDayOfMonth(this DateOnly dt) =>
-        dt.FirstDayOfMonth().AddMonths(1).AddDays(-1);
-
-    public static DateOnly FirstDayOfYear(this DateOnly dt) =>
-        new(dt.Year, 1, 1);
-
-    public static DateOnly LastDayOfYear(this DateOnly dt) =>
-        new(dt.Year, 12, 31);
 }
